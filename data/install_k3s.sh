69f54488c19d551f4fa28f2adfa52957814be81d
khushalchandak17 <khushal.chandak@suse.com>
2023-12-08 05:07:57 +0530

    Update install_k3s.sh

--- data/install_k3s.sh
+++ data/install_k3s.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+install_k3s() {
+  clear
+  echo -e "Installing k3s... \n Fetching all the avaialble version from upstream \n \n"
+  get_k3s_version
+  echo "$K3S_VERSION"
+  sleep 2
+
+  curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=$K3S_VERSION sh -
+
+  # Verify k3s installation
+  sudo k3s kubectl get nodes
+
+  mkdir ~/.kube/
+  cp /etc/rancher/k3s/k3s.yaml  ~/.kube/config
+  #export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+  for i in $(kubectl get deploy -n kube-system --no-headers | awk '{print $1}'); do  kubectl -n kube-system rollout status deploy $i; done
+
+}
+
+function get_k3s_version {
   # Get list of available k3s versions from GitHub API
   VERSIONS_URL="https://api.github.com/repositories/135516270/releases"
   VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | sort -rV)
@@ -16,3 +38,6 @@
   else
     K3S_VERSION=$VERSION
   fi
+}
+
+install_k3s
