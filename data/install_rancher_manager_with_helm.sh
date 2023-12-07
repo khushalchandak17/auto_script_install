aa5978baa7e1f518d84756391c7c2950ae5c979b
khushalchandak17 <khushal.chandak@suse.com>
2023-12-08 05:21:57 +0530

    Update install_rancher_manager_with_helm.sh

--- data/install_rancher_manager_with_helm.sh
+++ data/install_rancher_manager_with_helm.sh
@@ -1,55 +1,47 @@
 #!/bin/bash
+
 install_rancher() {
-clear
-echo -e "Installing Rancher Manager using Helm... \n Fetching all the avaialble version from upstream \n \n"
-get_rancher_version
-echo "Installing Rancher $RANCHER_VERSION"
-sleep 3
+  clear
+  echo -e "Installing Rancher Manager using Helm... \n Fetching all available versions from upstream \n \n"
+  get_rancher_version
+  echo "Installing Rancher $RANCHER_VERSION"
+  sleep 3
 
- ### Rancher Server Installation
+  # Rancher Server Installation
 
+  # Adding Helm-3
+  curl -#L https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-# Adding Helm-3
+  # Add needed helm charts
+  helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+  helm repo add jetstack https://charts.jetstack.io
 
-curl -#L https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+  # Add the cert-manager CRD
+  kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.crds.yaml
 
-# add needed helm charts
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-helm repo add jetstack https://charts.jetstack.io
+  # Helm install jetstack
+  helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace
 
+  # Installing Rancher Server
+  read -p "Enter your hostname: " hostname_rancher
+  echo "Your provided hostname is $hostname_rancher"
 
+  read -p "Password: " password_rancher
 
-# still on  rancher1 master, worker not configured yet
-# add the cert-manager CRD
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.crds.yaml
+  # To install a specific version of Rancher
+  # Get Rancher version first
+  # helm search repo rancher-latest --versions
 
-# helm install jetstack
-helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace
+  helm upgrade -i rancher rancher-latest/rancher --version $RANCHER_VERSION --create-namespace --namespace cattle-system --set hostname=${hostname_rancher} --set bootstrapPassword=${password_rancher} --set replicas=1
+  sleep 5
 
+  kubectl get pods -A
 
-### Installing Rancher Server
-
-
-read -p "Enter your hostname: " hostname_rancher
-echo "Your provided hostname is $hostname_rancher"
-
-read -p "Password: " password_rancher
-
-### To install specific version rancher
-## Get rancher version first
-# helm search repo rancher-latest --versions
-
-helm upgrade -i rancher rancher-latest/rancher --version $RANCHER_VERSION  --create-namespace --namespace cattle-system --set hostname=${hostname_rancher} --set bootstrapPassword=${password_rancher} --set replicas=1
-sleep 5
-
-kubectl get pods -A
-
-# Verify Rancher installation
-kubectl -n cattle-system rollout status deploy/rancher
-
+  # Verify Rancher installation
+  kubectl -n cattle-system rollout status deploy/rancher
 }
 
-get_rancher_version () {
+get_rancher_version() {
   # Get list of available Rancher versions from GitHub API
   VERSIONS_URL="https://api.github.com/repos/rancher/rancher/releases"
   VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | grep -v 'alpha\|beta' | sort -rV)
@@ -70,4 +62,5 @@ get_rancher_version () {
   fi
 }
 
+# Call the install_rancher function
 install_rancher
