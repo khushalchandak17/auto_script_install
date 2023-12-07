c9e404a894a5544b4666a3b74c1fd725405ca270
khushalchandak17 <khushal.chandak@suse.com>
2023-12-08 04:54:08 +0530

    Update install_rancher_manager_with_docker.sh

--- data/install_rancher_manager_with_docker.sh
+++ data/install_rancher_manager_with_docker.sh
@@ -1,1 +1,8 @@
-as
+echo "Installing Rancher Manager using Docker..."
+curl -fsSL get.docker.com | bash
+systemctl enable docker
+docker run --privileged -d --restart=no -p 8080:80 -p 8443:443 -p 36443:6443 -v rancher:/var/lib/rancher  rancher/rancher
+
+
+
+
