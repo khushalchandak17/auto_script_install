843706484b68090f646b3189a3cf1a272d7e397d
khushalchandak17 <khushal.chandak@suse.com>
2023-12-08 04:57:51 +0530

    Update install_k3s.sh

--- data/install_k3s.sh
+++ data/install_k3s.sh
@@ -1,1 +1,18 @@
+  # Get list of available k3s versions from GitHub API
+  VERSIONS_URL="https://api.github.com/repositories/135516270/releases"
+  VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | sort -rV)
 
+  # Display menu of available versions
+  echo -e "Please select a k3s version to install or use option 1 for the latest stable version: \n"
+  select VERSION in "latest" $VERSIONS ; do
+    if [ -n "$VERSION" ]; then
+      break
+    fi
+  done
+
+  # Set K3S_VERSION variable
+  if [ "$VERSION" = "latest" ]; then
+    K3S_VERSION=$(curl -s $VERSIONS_URL | grep '"name":' | cut -d '"' -f 4 | head -n 1)
+  else
+    K3S_VERSION=$VERSION
+  fi
