bfd118d35e12549b8a8849fe4cf271b9895e25b7
khushalchandak17 <khushal.chandak@suse.com>
2023-12-12 17:36:50 +0530

    Update install_rke2.sh

--- data/install_rke2.sh
+++ data/install_rke2.sh
@@ -39,39 +39,49 @@ sleep 2
 
 # Function to display menu for all avaialble RKE2 version and get user's choice
 function get_rke2_version {
+  # Get the system architecture
+  architecture=$(uname -m)
+
   # Get list of available RKE2 versions from GitHub API
-architecture=$(uname -m)
-VERSIONS_URL="https://api.github.com/repos/rancher/rke2/releases"
+  VERSIONS_URL="https://api.github.com/repos/rancher/rke2/releases"
 
-if [ "$architecture" == "x86_64" ] || [ "$architecture" == "amd64" ]; then
-  VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | sort -rV)
-elif [ "$architecture" == "arm" ] || [ "$architecture" == "aarch64" ]; then
-  VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | grep -E '^v(1\.27\.|1\.28\.)' | sort -rV)
-else
-  echo "Unsupported architecture '$architecture' detected. Quitting..."
-  exit 1
-fi
+  if [ "$architecture" == "x86_64" ] || [ "$architecture" == "amd64" ]; then
+    VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | sort -rV)
+  elif [ "$architecture" == "arm" ] || [ "$architecture" == "aarch64" ]; then
+    VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | grep -E '^v(1\.27\.|1\.28\.)' | sort -rV)
+  else
+    echo "Unsupported architecture '$architecture' detected. Quitting..."
+    exit 1
+  fi
 
-
-  # Add "Latest" option to the list
-  VERSIONS="Latest $VERSIONS"
+  # Add "Latest" and "Other" options to the list
+  VERSIONS="Latest Other $VERSIONS"
 
   # Display menu of available versions
-  echo -e "Please select an RKE2 version to install or use option 1 for the latest stable version: \n"
+  echo -e "Please select an RKE2 version to install or choose 'Other' to enter a specific version: \n"
   select VERSION in $VERSIONS; do
     if [ -n "$VERSION" ]; then
       break
     fi
   done
 
-  # Check if "Latest" option was selected
-  if [ "$VERSION" = "Latest" ]; then
-    # Fetch the latest version from the GitHub API
-    LATEST_VERSION=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | head -n 1)
-    RKE2_VERSION=$LATEST_VERSION
-  else
-    # Set RKE2_VERSION variable to the selected version
-    RKE2_VERSION=$VERSION
-  fi
+  # Check if "Latest" or "Other" option was selected
+  case $VERSION in
+    "Latest")
+      # Fetch the latest version from the GitHub API
+      LATEST_VERSION=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | head -n 1)
+      RKE2_VERSION=$LATEST_VERSION
+      ;;
+    "Other")
+      # Prompt the user to enter a specific version
+      read -p "Enter the RKE2 version you want to install: " CUSTOM_VERSION
+      RKE2_VERSION=$CUSTOM_VERSION
+      ;;
+    *)
+      # Set RKE2_VERSION variable to the selected version
+      RKE2_VERSION=$VERSION
+      ;;
+  esac
 }
+
 install_rke2
