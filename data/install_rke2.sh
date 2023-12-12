e4bd0594fdbc5fa4f9c858589d950d33d5fcf2f7
khushalchandak17 <khushal.chandak@suse.com>
2023-12-12 19:30:34 +0530

    Update install_rke2.sh

--- data/install_rke2.sh
+++ data/install_rke2.sh
@@ -1,91 +1,128 @@
-#!/bin/bash
-
-install_k3s() {
+install_rke2() {
   clear
-  echo -e "Installing k3s... \n Fetching all the available versions from upstream \n \n"
-  get_k3s_version
-  echo "$K3S_VERSION"
+
+  echo -e "Installing RKE2 Server ... \nFetching all the available versions from upstream \n \n"
+  # Add your installation logic for RKE2 Server using apt here
+  get_rke2_version
+  echo "$RKE2_VERSION"
   sleep 2
 
-  if [ "$K3S_VERSION" == "Other" ]; then
-    read -p "Enter the k3s version you want to install: " CUSTOM_VERSION
+  ## Using script not apt
+  # On rancher1
+  curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=server INSTALL_RKE2_CHANNEL=$RKE2_VERSION sh -
 
-    if ! validate_k3s_version "$CUSTOM_VERSION"; then
-      echo
-      echo "Version not found: $CUSTOM_VERSION"
-      sleep 2
-      echo "Verify k3s Version i.e $CUSTOM_VERSION"
-      sleep 5
-      clear
+  # start and enable for restarts -
+  echo -e "\nInitializing RKE2 Server"
+  systemctl enable --now rke2-server.service
 
-      install_k3s
-    fi
+  systemctl status rke2-server --no-pager
 
-    K3S_VERSION=$CUSTOM_VERSION
-  fi
-
-  curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=$K3S_VERSION sh -
-
-  # Verify k3s installation
-  sudo k3s kubectl get nodes
+  cp "$(find /var/lib/rancher/rke2/data/ -name kubectl)" /usr/local/bin/kubectl
+  chmod +x /usr/local/bin/kubectl
 
   mkdir -p ~/.kube/
-  cp /etc/rancher/k3s/k3s.yaml  ~/.kube/config
-  #export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+  cp /etc/rancher/rke2/rke2.yaml  ~/.kube/config
+  #export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
+
+  kubectl version --short
+
+  kubectl get node -o wide
+  sleep 8
 
   for i in $(kubectl get deploy -n kube-system --no-headers | awk '{print $1}'); do
     kubectl -n kube-system rollout status deploy $i
   done
+
+  sleep 2
 }
 
-function validate_k3s_version() {
-  local K3S_VERSION=$1
+# Function to display a menu for all available RKE2 versions and get the user's choice
 
-    if [ "$architecture" == "x86_64" ]; then
+
+validate_url() {
+  local RKE_VERSION=$1
+
+  if [ "$architecture" == "x86_64" ]; then
     echo "AMD architecture detected. Continuing with AMD-specific actions..."
-    RKE_arc=k3s
+    RKE_arc=rke2.linux-amd64
   else
     echo "ARM architecture detected. Continuing with ARM-specific actions..."
-    RKE_arc=k3s-arm64
+    RKE_arc=rke2.linux-arm64
   fi
 
-  K3S_DOWNLOAD_URL="https://github.com/k3s-io/k3s/releases/download/$K3S_VERSION/$RKE_arc"
+  RKE_DOWNLOAD_URL="https://github.com/rancher/rke2/releases/download/$RKE_VERSION/$RKE_arc"
 
-  if wget --spider "$K3S_DOWNLOAD_URL" 2>/dev/null; then
+  if wget --spider "$RKE_DOWNLOAD_URL" 2>/dev/null; then
     return 0  # URL is reachable
   else
     return 1  # URL is not reachable
   fi
 }
 
-function get_k3s_version {
-  # Get list of available k3s versions from GitHub API
-  VERSIONS_URL="https://api.github.com/repositories/135516270/releases"
-  VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | sort -rV)
+function get_rke2_version {
+  # Get the system architecture
+  architecture=$(uname -m)
+
+  # Get list of available RKE2 versions from GitHub API
+  VERSIONS_URL="https://api.github.com/repos/rancher/rke2/releases"
+
+  if [ "$architecture" == "x86_64" ] || [ "$architecture" == "amd64" ]; then
+    VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | sort -rV)
+  elif [ "$architecture" == "arm" ] || [ "$architecture" == "aarch64" ]; then
+    VERSIONS=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | grep -E '^v(1\.27\.|1\.28\.)' | sort -rV)
+  else
+    echo "Unsupported architecture '$architecture' detected. Quitting..."
+    exit 1
+  fi
 
   # Add "Latest" and "Other" options to the list
   VERSIONS="Latest Other $VERSIONS"
 
-  # Display menu of available versions
-  echo -e "Please select a k3s version to install or choose 'Other' to enter a specific version: \n"
-  select VERSION in $VERSIONS ; do
-    if [ -n "$VERSION" ]; then
-      break
-    fi
-  done
+  while true; do
+    # Display menu of available versions
+    echo -e "Please select an RKE2 version to install or choose 'Other' to enter a specific version: \n"
+    select VERSION in $VERSIONS; do
+      if [ -n "$VERSION" ]; then
+        break
+      fi
+    done
 
-  # Set K3S_VERSION variable
-  case $VERSION in
-    "Latest")
-      K3S_VERSION=$(curl -s $VERSIONS_URL | grep '"name":' | cut -d '"' -f 4 | head -n 1)
-      ;;
-    "Other")
-      K3S_VERSION="Other"
-      ;;
-    *)
-      K3S_VERSION=$VERSION
-      ;;
-  esac
+    # Check if "Latest" or "Other" option was selected
+    case $VERSION in
+      "Latest")
+        # Fetch the latest version from the GitHub API
+        LATEST_VERSION=$(curl -s $VERSIONS_URL | grep '"tag_name":' | cut -d '"' -f 4 | head -n 1)
+        RKE2_VERSION=$LATEST_VERSION
+        break
+        ;;
+      "Other")
+        # Prompt the user to enter a specific version
+        read -p "Enter the RKE2 version you want to install: " CUSTOM_VERSION
+
+        # Validate the entered version
+        if ! validate_url "$CUSTOM_VERSION"; then
+          echo
+	  echo
+	  echo "Version not found: $CUSTOM_VERSION"
+          sleep 2
+          echo "Verify RKE Version i.e  $CUSTOM_VERSION"
+          sleep 5
+	  clear
+          continue  # Continue the loop to prompt the user again
+        fi
+
+        # If validation is successful, break out of the loop
+        RKE2_VERSION=$CUSTOM_VERSION
+        break
+        ;;
+      *)
+        # Set RKE2_VERSION variable to the selected version
+        RKE2_VERSION=$VERSION
+        break
+        ;;
+    esac
+  done
 }
 
-install_k3s
+
+install_rke2
